359cc4bd074101865c25f455c5a51312f90f09ee
metherton <martinetherton63@gmail.com>
2017-03-13 23:27:10 +0100

    add styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,16 @@
-/**
- * Created by martin on 13/03/17.
- */
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppShoppingcartComponent } from './app-shoppingcart/app-shoppingcart.component'
+
+const routes: Routes = [
+  {path: 'shoppingcart', component: AppShoppingcartComponent}
+];
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppShoppingcartComponent } from './app-shoppingcart/app-shoppingcart.component';
+import { AppTodoItemComponent } from './app-todo-item/app-todo-item.component';
+import { AppAddItemComponent } from './app-add-item/app-add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppShoppingcartComponent,
+    AppTodoItemComponent,
+    AppAddItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
